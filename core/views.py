07a4560b02cdc6c32b5783d244e4f79c22f7e39d
BehnamDadashi <kryptickourosh@gmail.com>
2026-02-27 13:51:57 +0330

    Another Test

--- core/views.py
+++ core/views.py
@@ -2,5 +2,5 @@ from django.http import JsonResponse
 
 def home(request):
     return JsonResponse(
-        {'message': 'This change has to be made on the host!'}
+        {'message': 'another check!'}
     )
